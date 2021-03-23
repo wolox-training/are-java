39af8398b213d3e5811529207f4c29ca89f47e6b
alisonreynoso <alison.reynoso@wolox.com.ar>
2021-03-23 13:45:10 -0300

    Query method in BookRepository that searches by  authors now return optional

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,11 +1,12 @@
 package wolox.training.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
 public interface BookRepository extends Repository<Book, Long> {
 
-  List<Book> findByAuthor(String author);
+  Optional<Book> findFirstByAuthor(String author);
 
 }
