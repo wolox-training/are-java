91928fd454c9e7e4f3a64d13603366f4cf2308be
alisonreynoso <alison.reynoso@wolox.com.ar>
2021-03-24 11:46:42 -0300

    The method query that searches users by name in the userRepository now returns optional

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package wolox.training.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-  List<User> findByName(String name);
+  Optional<User> findFirstByName(String name);
 
 }
