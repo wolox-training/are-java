4f6463ea98f1d182117425c8722e257b75667dd6
alisonreynoso <alison.reynoso@wolox.com.ar>
2021-04-15 12:30:51 -0300

    Now the user, student and professor class have a field that indicates their type of user

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package wolox.training.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,7 +13,5 @@ import wolox.training.models.Professor;
 import wolox.training.models.User;
 
 public interface UserRepository extends UserBaseRepository<User>, CrudRepository<User, Long> {
-
-
 
 }
--- src/test/java/wolox/training/models/DifferentTypeOfUsersTest.java
+++ src/test/java/wolox/training/models/DifferentTypeOfUsersTest.java
@@ -1,11 +1,11 @@
 package wolox.training.models;
 
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -18,9 +18,11 @@ import wolox.training.models.UtilsForTest.DifferentTypeOfUsersForTest;
 import wolox.training.repositories.ProfessorRepository;
 import wolox.training.repositories.StudentRepository;
 import wolox.training.repositories.UserRepository;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class DifferentTypeOfUsersTest {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -33,43 +35,75 @@ public class DifferentTypeOfUsersTest {
     private List<User> justUsersList;
     private List<Professor> professorList;
     private List<Student> studentList;
+    private int totalUsers;
 
     @BeforeEach
-    void before(){
-        DifferentTypeOfUsersForTest differentTypeOfUsersForTest= new DifferentTypeOfUsersForTest();
-        justUsersList= differentTypeOfUsersForTest.usersList();
-        professorList= differentTypeOfUsersForTest.professorList();
-        studentList= differentTypeOfUsersForTest.studentList();
+    void before() {
+        DifferentTypeOfUsersForTest differentTypeOfUsersForTest = new DifferentTypeOfUsersForTest();
+        justUsersList = differentTypeOfUsersForTest.usersList();
+        professorList = differentTypeOfUsersForTest.professorList();
+        studentList = differentTypeOfUsersForTest.studentList();
         justUsersList.forEach(user -> userRepository.save(user));
         professorList.forEach(professor -> professorRepository.save(professor));
         studentList.forEach(student -> studentRepository.save(student));
+        totalUsers = justUsersList.size() + studentList.size() + professorList.size();
+
     }
+
     @Test
-    void whenSearchForAllTypeOfUsers_thenTheyAreRetrieved(){
-        int totalUsers= justUsersList.size()+studentList.size()+ professorList.size();
-        Pageable pageable= PageRequest.of(0,totalUsers+100);
-        List<User> users =userRepository.findAll(pageable).toList();
-        assertEquals(totalUsers,users.size());
+    void whenSearchForAllTypeOfUsers_thenStudentsAreRetrievedWithItsCorrectTypeOfUserField() {
+        Pageable pageable = PageRequest.of(0, totalUsers + 100);
+        List<User> users = userRepository.findAll(pageable).toList();
+        List<User> studentsUsers = users.stream().filter(user -> user.getTypeOfUser().equals("Student"))
+                .collect(Collectors.toList());
+        assertEquals(studentsUsers.size(),studentList.size());
+    }
+
+    @Test
+    void whenSearchForAllTypeOfUsers_thenProfessorsAreRetrievedWithItsCorrectTypeOfUserField() {
+        Pageable pageable = PageRequest.of(0, totalUsers + 100);
+        List<User> users = userRepository.findAll(pageable).toList();
+        List<User> professorsUsers = users.stream().filter(user -> user.getTypeOfUser().equals("Professor"))
+                .collect(Collectors.toList());
+        assertEquals(professorsUsers.size(),professorList.size());
+    }
+
+    @Test
+    void whenSearchForAllTypeOfUsers_thenUsersAreRetrievedWithItsCorrectTypeOfUserField() {
+        Pageable pageable = PageRequest.of(0, totalUsers + 100);
+        List<User> allUsers = userRepository.findAll(pageable).toList();
+        List<User> justUsers = allUsers.stream().filter(user -> user.getTypeOfUser().equals("User"))
+                .collect(Collectors.toList());
+        assertEquals(justUsers.size(),justUsersList.size());
+    }
+
+    @Test
+    void whenSearchForAllTypeOfUsers_thenTheyAreRetrieved() {
+        int totalUsers = justUsersList.size() + studentList.size() + professorList.size();
+        Pageable pageable = PageRequest.of(0, totalUsers + 100);
+        List<User> users = userRepository.findAll(pageable).toList();
+        assertEquals(totalUsers, users.size());
         assertTrue(users.containsAll(professorList));
         assertTrue(users.containsAll(studentList));
         assertTrue(users.containsAll(justUsersList));
     }
 
     @Test
-    void whenSearchForStudents_thenTheyAreRetrieved(){
-        int totalStudents= studentList.size();
-        Pageable pageable= PageRequest.of(0,totalStudents+100);
-        List<Student> students =studentRepository.findAll(pageable).toList();
-        assertEquals(totalStudents,students.size());
+    void whenSearchForStudents_thenTheyAreRetrieved() {
+        int totalStudents = studentList.size();
+        Pageable pageable = PageRequest.of(0, totalStudents + 100);
+        List<Student> students = studentRepository.findAll(pageable).toList();
+        assertEquals(totalStudents, students.size());
         assertTrue(students.containsAll(studentList));
     }
 
     @Test
-    void whenSearchForProfessors_thenTheyAreRetrieved(){
-        int totalProfessors= professorList.size();
-        Pageable pageable= PageRequest.of(0,totalProfessors+100);
-        List<Professor> professors =professorRepository.findAll(pageable).toList();
-        assertEquals(totalProfessors,professors.size());
+    void whenSearchForProfessors_thenTheyAreRetrieved() {
+        int totalProfessors = professorList.size();
+        Pageable pageable = PageRequest.of(0, totalProfessors + 100);
+        List<Professor> professors = professorRepository.findAll(pageable).toList();
+        assertEquals(totalProfessors, professors.size());
         assertTrue(professors.containsAll(professorList));
     }
+
 }
